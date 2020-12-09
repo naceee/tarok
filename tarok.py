b829500496ec642475bece4e6aa66b092f607d58
naceee <47631585+naceee@users.noreply.github.com>
2020-12-09 22:56:17 +0100

    edited python file

--- tarok.py
+++ tarok.py
@@ -1,36 +1,32 @@
 fileigre = open("igre.txt", "r")
-fileigralci = []
-fileigralci.append(open("peter.txt", "r"))
-fileigralci.append(open("nace.txt", "r"))
-fileigralci.append(open("blaz.txt", "r"))
-fileigralci.append(open("gasper.txt", "r"))
-fileigralci.append(open("jernej.txt", "r"))
-fileigralci.append(open("klancar.txt", "r"))
 
+fileigralci = []
 igre = []
-igralci = ["peter", "nace", "blaz", "gasper", "jernej", "klancar"]
 tocke =  []
+
+igralci = ["peter", "nace", "blaz", "gasper", "jernej", "klancar"]
 for i in range(len(igralci)):
 	tocke.append([])
+	fileigralci.append(open(igralci[i] + ".txt", "r"))
 
-print(tocke)
   
 while True: 
 	line = fileigre.readline()
-	print("---->", line)
 	if not line:
 		break
 	else:
 		igre.append(int(line))
 		for i in range(len(igralci)):
 			t = fileigralci[i].readline()
-			print(t)
 			if t != "\n":
 				tocke[i].append(int(t))
 			else:
-				tocke[i].append(0)				
+				tocke[i].append(0)	
 
+			
 
+print("stevilo iger")
+print(igre)
 for i in range(len(igralci)):
 	print(igralci[i])
 	print(tocke[i])
